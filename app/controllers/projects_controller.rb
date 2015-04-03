596081d8e68fa3bf8e9ed6f8f96b8df0acc5ea66
Takashi Michihiro <michihiro@ken>
2015-04-03 14:32:29 +0900

    moved controller logic from application_controller to project_controller, and fixed the bug where the tasks are not deleted even a project is deleted

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,50 @@
 class ProjectsController < ApplicationController
+
+  def index
+    @projects = Project.all
+  end
+
+  def show
+    @projects = Project.find(params[:id])
+  end
+
+  def new
+    @projects = Project.new
+  end
+
+  def create
+    @projects = Project.new(project_params)
+    if @projects.save
+      redirect_to projects_path
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @projects = Project.find(params[:id])
+  end
+
+  def update
+    @projects = Project.find(params[:id])
+    if @projects.update(project_params)
+      redirect_to projects_path
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @projects = Project.find(params[:id])
+    @projects.destroy
+    redirect_to projects_path
+  end
+
+  private
+
+  def project_params
+    params[:project].permit(:title)
+  end
+
+
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,5 @@
 class Project < ActiveRecord::Base
-  has_many :tasks
+  has_many :tasks, dependent: :destroy
   validates :title,
   presence: { message: "Please imput the project title" },
   length: {minimum: 2, message: "Please imput at least 2 charactor" }
