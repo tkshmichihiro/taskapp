2580697cf483d21a34a876e29d86ae5e5f83e6a0
Takashi Michihiro <michihiro@ken>
2015-04-03 14:12:34 +0900

    completed taskapp

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
@@ -13,6 +14,40 @@ class ApplicationController < ActionController::Base
 
   def new
     @projects = Project.new
+  end
+
+  def create
+    @projects = Project.new(project_params)
+    if @projects.save
+      redirect_to projects_path
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @projects = Project.find(params[:id])
+  end
+
+  def update
+    @projects = Project.find(params[:id])
+    if @projects.update(project_params)
+      redirect_to projects_path
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @projects = Project.find(params[:id])
+    @projects.destroy
+    redirect_to projects_path
+  end
+
+  private
+
+  def project_params
+    params[:project].permit(:title)
   end
 
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,6 @@
 class Project < ActiveRecord::Base
+  has_many :tasks
+  validates :title,
+  presence: { message: "Please imput the project title" },
+  length: {minimum: 2, message: "Please imput at least 2 charactor" }
 end
